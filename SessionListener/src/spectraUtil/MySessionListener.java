b2dc36bc81cd2aff9cbdbad5f7d81a18887eaf43
ace540i <michael.darretta@fmc-na.com>
2018-10-24 19:56:24 -0400

    Add files via upload

---
+++ SessionListener/src/spectraUtil/MySessionListener.java
@@ -0,0 +1,43 @@
+package spectraUtil;
+
+ 
+ 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+ 
+public class MySessionListener implements HttpSessionListener {
+	 private static int activeSessions = 0;
+	public void sessionCreated(HttpSessionEvent event) {
+		activeSessions++;
+		System.out.println("A new session is created");
+	
+//		   String queries = "";
+//		   Enumeration e = event.getSession().getAttributeNames();
+//
+//		   if ( e.hasMoreElements() )
+//		   {
+//		       queries += "<h4>Recent Queries</h4><ul>";
+//		   }
+//
+//		   while ( e.hasMoreElements() )
+//		   {
+//		       String name = (String) e.nextElement();
+//		       String value = (String) event.getSession().getAttribute(name);
+//		       System.out.println(value +" name= " + name );
+//		   }
+		getActiveSessions();
+	}
+ 
+	public void sessionDestroyed(HttpSessionEvent event) {
+		  if(activeSessions > 0)
+		      activeSessions--;
+		System.out.println("session is destroyed");
+		getActiveSessions();
+	}
+public static int getActiveSessions() {
+	System.out.println("activeSessions = "+activeSessions);
+    return activeSessions;
+ }
+}
---
+++ SessionListener1/src/spectraUtil/LoginServlet.java
@@ -0,0 +1,36 @@
+package spectraUtil;
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet { 
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		HttpSession session = null;
+		RequestDispatcher dispatcher = null;
+		if (("test".equalsIgnoreCase(username) && "test".equals(password))) {
+			session = request.getSession();
+			session.setAttribute("username", username);
+			dispatcher = request.getRequestDispatcher("Welcome.jsp");
+		} else {
+			dispatcher = request.getRequestDispatcher("failure.jsp");
+		}
+		dispatcher.forward(request, response);
+	}
+}
